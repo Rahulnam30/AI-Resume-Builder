6f5328aa7ac97f30a4dda5f9c10a9488fd70a60a
juneyear21 <juneyear21@gmail.com>
2026-01-10 16:46:32 +0530

    feat: add paid users table and plan field

--- backend/Models/User.js
+++ backend/Models/User.js
@@ -22,6 +22,10 @@ const userSchema = new mongoose.Schema(
       type: Boolean,
       default: true,
     },
+    plan: {
+      type: String,
+      default: "Free",
+    },
   },
   { timestamps: true }
 );
