e267d4316d310d189171470d6b8107cd75d4fd87
Alok Ranjan <iitpatnaalokranjan@gmail.com>
2026-03-05 13:35:22 +0530

    fixed navbar double download ntn showing issue

--- frontend/src/components/user/CV/Cvbuildernavbar.jsx
+++ frontend/src/components/user/CV/Cvbuildernavbar.jsx
@@ -295,7 +295,7 @@ const CVBuilderTopBar = ({
       </div>
 
       {/* Right Side: Actions */}
-      <div className="flex flex-wrap justify-center md:justify-end items-center gap-2 w-full md:w-auto">
+      {/* <div className="flex flex-wrap justify-center md:justify-end items-center gap-2 w-full md:w-auto">
         <button className="items-center gap-2 px-4 py-2 rounded-lg hidden md:flex border border-gray-300 bg-white text-gray-800 font-medium shadow-sm hover:bg-black hover:text-white transition-all duration-200 select-none">
           <PenTool size={18} />
           CV Designer
@@ -318,7 +318,7 @@ const CVBuilderTopBar = ({
           )}
           <span className="hidden md:inline">{isSaving ? "Saving..." : "Download"}</span>
         </button>
-      </div>
+      </div> */}
     </div>
   );
 };
