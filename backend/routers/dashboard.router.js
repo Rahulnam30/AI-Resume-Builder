5be1d08f2b923af35506826515892f915b0f2278
Rahulnam30 <nambiarrahul301104@gmail.com>
2026-03-28 16:49:19 +0530

    Revert "Preview Changes"
    This reverts commit be4a52105635a0d5622cf1241f3d66a79d4ed3f6.

--- backend/routers/dashboard.router.js
+++
@@ -1,9 +0,0 @@
-import express from "express";
-import isAuth from "../middlewares/isAuth.js";
-import { getDashboardSummary } from "../controllers/dashboard.controller.js";
-
-const dashboardRouter = express.Router();
-
-dashboardRouter.get("/summary", isAuth, getDashboardSummary);
-
-export default dashboardRouter;
--- backend/routers/export.router.js
+++
@@ -1,8 +0,0 @@
-import express from 'express';
-import { exportFile } from '../controllers/export.controller.js';
-
-const router = express.Router();
-
-router.post('/export', exportFile);
-
-export default router;
