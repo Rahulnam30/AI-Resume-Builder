50a9dfefe8efb8fafe961cdfc73c38aae6de1c14
Krishna Darsh E K <krishnabackup2002@gmail.com>
2026-04-03 15:04:08 +0530

    refactor: added maxclient , idleTimeout and connectionTimeout in main db config file

--- backend/config/db.js
+++ backend/config/db.js
@@ -1,10 +1,22 @@
 import pg from "pg";
+import dotenv from "dotenv";
+import dns from 'dns';
+
+
+dns.setDefaultResultOrder('ipv4first');
+
+dotenv.config();
+
 
 const { Pool } = pg;
 
-export const pool = new Pool({
-  connectionString: process.env.POSTGRESQL_URI,
+export const pool = new  Pool({
+    connectionString : process.env.POSTGRESQL_URI,
+    max: 10, // Limit active connections to prevent Supabase MaxClientsInSessionMode error
+    idleTimeoutMillis: 30000, // Close idle connections after 30 seconds
+    connectionTimeoutMillis: 5000,
 });
+
 
 const connectDB = async () => {
   try {
